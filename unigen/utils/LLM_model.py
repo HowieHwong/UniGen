e562c0ebd62de66e6e2655fdbd8102bf788297be
Huang Yue <1431039261@qq.com>
2024-06-08 15:44:43 +0800

    update

--- unigen/utils/LLM_model.py
+++ unigen/utils/LLM_model.py
@@ -1,23 +1,23 @@
-
 from tenacity import retry, wait_random_exponential, stop_after_attempt
-from openai import OpenAI,AzureOpenAI
-from  anthropic import Anthropic
+from openai import OpenAI, AzureOpenAI
+from anthropic import Anthropic
 import traceback
 from .configuration import ConfigManager
-config=ConfigManager.get_config_dict()
+
+config = ConfigManager.get_config_dict()
 
 
 class ModelAPI:
-    def __init__(self, model_type='gpt',temperature=0.8):
+    def __init__(self, model_type='gpt', temperature=0.8):
         self.model_type = model_type.lower()
-        self.temperature=temperature
-        
-        if self.model_type not in ['gpt', 'claude','llama3']:
+        self.temperature = temperature
+
+        if self.model_type not in ['gpt', 'claude', 'llama3']:
             raise ValueError(f"Unsupported model type: {model_type}")
-    
+
     @retry(wait=wait_random_exponential(min=1, max=10), stop=stop_after_attempt(10))
     def get_res(self, text, model=None, message=None, azure=True, json_format=False, temperature=0.8):
-        temperature=self.temperature
+        temperature = self.temperature
         try:
             if self.model_type.lower() == 'gpt':
                 self.api_key = config['api_settings']['openai_api']
@@ -32,16 +32,16 @@ class ModelAPI:
                 raise ValueError(f"Unsupported model type: {self.model_type}")
         except Exception as e:
             print(traceback.format_exc())
-    
-    def api_send_llama3(self,string, model=None, message=None, json_format=False,temperature=0.8):
+
+    def api_send_llama3(self, string, model=None, message=None, json_format=False, temperature=0.8):
         client = OpenAI(api_key=self.api_key,
-                            base_url="https://api.deepinfra.com/v1/openai",
-                            )
-        top_p=1 if temperature<=1e-5 else 0.9
-        temperature=0.01 if temperature<=1e-5 else temperature
-        model='meta-llama/Meta-Llama-3-70B-Instruct'
+                        base_url="https://api.deepinfra.com/v1/openai",
+                        )
+        top_p = 1 if temperature <= 1e-5 else 0.9
+        temperature = 0.01 if temperature <= 1e-5 else temperature
+        model = 'meta-llama/Meta-Llama-3-70B-Instruct'
         print(f"Sending API request...{model}")
-        chat_completion= client.chat.completions.create(
+        chat_completion = client.chat.completions.create(
             model='meta-llama/Meta-Llama-3-70B-Instruct',
             messages=[{"role": "user", "content": string}],
             temperature=temperature,
@@ -52,12 +52,11 @@ class ModelAPI:
             raise ValueError("The response from the API is NULL or an empty string!")
         return chat_completion.choices[0].message.content
 
-
-    def api_send_claude(self,string, model="claude-3-opus-20240229", message=None, json_format=False,temperature=0.8):
+    def api_send_claude(self, string, model="claude-3-opus-20240229", message=None, json_format=False, temperature=0.8):
         client = Anthropic(
             api_key=self.api_key,
         )
-        model="claude-3-opus-20240229",
+        model = "claude-3-opus-20240229",
         print(f"Sending API request...{model}")
         message = client.messages.create(
             temperature=temperature,
@@ -72,10 +71,10 @@ class ModelAPI:
         )
         if not message.choices[0].message.content:
             raise ValueError("The response from the API is NULL or an empty string!")
-        full_response=message.content[0].text
+        full_response = message.content[0].text
         return full_response
-    
-    def api_send_gpt4(self,string, model, message=None, azure=True, json_format=False,temperature=0.8):
+
+    def api_send_gpt4(self, string, model, message=None, azure=True, json_format=False, temperature=0.8):
         azure = config["generation_settings"]["azure_openai"]
         if message is None:
             message = [{"role": "user", "content": string}]
@@ -100,12 +99,12 @@ class ModelAPI:
                 response_format=response_format,
             )
         else:
-            model=config["generation_settings"]["openai_chat_model"]
+            model = config["generation_settings"]["openai_chat_model"]
             base_url = config["generation_settings"]["base_url"]
             client = OpenAI(api_key=self.api_key,
                             #base_url=base_url,
                             )
-            model="gpt-4-0125-preview"
+            model = "gpt-4-0125-preview"
             print(f"Sending API request...{model},temperature:{temperature}")
             chat_completion = client.chat.completions.create(
                 model=model,
@@ -113,8 +112,8 @@ class ModelAPI:
                 temperature=temperature,
                 response_format=response_format,
             )
-        dict(stream.usage) #{'completion_tokens': 28, 'prompt_tokens': 12, 'total_tokens': 40}
+        dict(stream.usage)  #{'completion_tokens': 28, 'prompt_tokens': 12, 'total_tokens': 40}
         if not chat_completion.choices[0].message.content:
             raise ValueError("The response from the API is NULL or an empty string!")
-        full_response=chat_completion.choices[0].message.content
+        full_response = chat_completion.choices[0].message.content
         return full_response
--- unigen/utils/self_reflection.py
+++ unigen/utils/self_reflection.py
@@ -2,28 +2,26 @@
 # -*- coding: utf-8 -*-
 from . import data_format
 from .data_format import prompt_format
-from .IO import print,input
+from .IO import print, input
 import json
 import copy
 from queue import Queue
 from concurrent.futures import ThreadPoolExecutor
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 from .prompt import prompt_template
+from concurrent.futures import ThreadPoolExecutor
+import traceback
 
-
-
-ans_format={
-"reflection":"""(If isgood is "yes", include reasons here. If "no", include a detailed analysis here.)""",
-"isgood":"yes/no",
+ans_format = {
+    "reflection": """(If isgood is "yes", include reasons here. If "no", include a detailed analysis here.)""",
+    "isgood": "yes/no",
 }
 
-few_shot_examples='''Few Shot Examples:
+few_shot_examples = '''Few Shot Examples:
 [[few_shot_des]]
 '''
 
-
-
-reflection_generation='''You are a professional dataset generation assistant. Your task is to assess the quality of the provided Data Entry based on dataset description,few shot examples and criteria such as quality, format, relevance, accuracy, and challenge level. 
+reflection_generation = '''You are a professional dataset generation assistant. Your task is to assess the quality of the provided Data Entry based on dataset description,few shot examples and criteria such as quality, format, relevance, accuracy, and challenge level. 
 DATASET DESCRIPTION:[[description]]
 
 [[few_shot_examples]]
@@ -39,10 +37,9 @@ Your assessment and reflection must be formatted as following JSON:
 [[ans_format]]
 Directly output your improved example as the following JSON format:'''
 
-reflection_generation=reflection_generation.replace("[[ans_format]]",json.dumps(ans_format))
+reflection_generation = reflection_generation.replace("[[ans_format]]", json.dumps(ans_format))
 
-
-improve_examples_with_reflection='''You are a professional dataset generation assistant.
+improve_examples_with_reflection = '''You are a professional dataset generation assistant.
 Your task is to create improved versions of the original example based on the reflection and ,few shot examples.
 
 DATASET DESCRIPTION:[[description]].
@@ -62,101 +59,103 @@ Reflection:[[reflection]]
 
 The structure and form of the improved example should be SAME with the original example; DO NOT make significant changes to the existing example. Directly output your improved Data Entry as the following JSON format:'''
 
-prompt_template['reflection_generation']=reflection_generation
-prompt_template['improve_examples_with_reflection']=improve_examples_with_reflection
-prompt_template['few_shot_examples']=few_shot_examples
+prompt_template['reflection_generation'] = reflection_generation
+prompt_template['improve_examples_with_reflection'] = improve_examples_with_reflection
+prompt_template['few_shot_examples'] = few_shot_examples
 
 
 @retry(wait=wait_random_exponential(min=2, max=3), stop=stop_after_attempt(3))
-def reflection_improve_example(example,dataset_description,few_shot_des,constraint):
-    reflection_epoch_trajectory=[]
+def reflection_improve_example(example, dataset_description, few_shot_des, constraint):
+    reflection_epoch_trajectory = []
     safe_example = copy.deepcopy(example)
     reflection_epoch_trajectory.append(safe_example)
-    
-    few_shot=prompt_format(prompt_template['few_shot_examples'],
-                           few_shot_des=few_shot_des)
+
+    few_shot = prompt_format(prompt_template['few_shot_examples'],
+                             few_shot_des=few_shot_des)
     prompt = prompt_format(prompt_template['reflection_generation'],
                            description=dataset_description,
                            example=example,
                            constraint=constraint,
-                           few_shot_examples=few_shot,)
-    reflection_data=data_format.get_res_data(prompt)
-    isgood=reflection_data.get("isgood", "Unknown")== "yes"
+                           few_shot_examples=few_shot, )
+    reflection_data = data_format.get_res_data(prompt)
+    isgood = reflection_data.get("isgood", "Unknown") == "yes"
     reflection_epoch_trajectory.append(reflection_data)
-    
+
     if (isgood):
-        print('----------------------------No need to improve----------------------------',
-                "GREEN")
+        print("*" * 15 + 'No need to improve' + "*" * 15,
+              "GREEN")
     else:
-        print('----------------------------Example Need to be Improved----------------------------',
-                "GREEN")
+        print("*" * 15 + 'Example Need to be Improved' + "*" * 15,
+              "GREEN")
         print(example["text"])
-        print("------------------------------------------------------------------------------------",
-                "GREEN")
+        print("*" * 40,
+              "GREEN")
         ## improve procedure
-            
-        few_shot=prompt_format(prompt_template['few_shot_examples'],
-                            few_shot_des=few_shot_des)
+
+        few_shot = prompt_format(prompt_template['few_shot_examples'],
+                                 few_shot_des=few_shot_des)
         prompt = prompt_format(prompt_template['improve_examples_with_reflection'],
                                description=dataset_description,
                                reflection=reflection_data['reflection'], original_example=example,
                                constraint=constraint,
-                               few_shot_examples=few_shot,)
-        with_label="label" in example.keys()
+                               few_shot_examples=few_shot, )
+        with_label = "label" in example.keys()
         data_format_str = data_format.data_entry_format(with_label=with_label, el_num=1)
-        prompt+=data_format_str
-        
-        improved_example=data_format.get_res_data(prompt)
-        improved_example=improved_example[0]
-        
+        prompt += data_format_str
+
+        improved_example = data_format.get_res_data(prompt)
+        improved_example = improved_example[0]
+
         reflection_epoch_trajectory.append(improved_example)
-        
-        example['text']=improved_example["text"]
+
+        example['text'] = improved_example["text"]
         if with_label:
             example['label'] = improved_example["label"]
 
-        print('----------------------------Example After Improvement-----------------------------', "GREEN")
+        print("*" * 15 + 'Example After Improvement' + "*" * 15, "GREEN")
         print(example["text"])
-        print("----------------------------------------------------------------------------------", "GREEN")
-    return example,isgood,reflection_epoch_trajectory
+        print("*" * 40, "GREEN")
+    return example, isgood, reflection_epoch_trajectory
 
 
-        
-from concurrent.futures import ThreadPoolExecutor
-
-def self_reflection(examples, dataset_description,few_shot_des,constraint, max_reflection=5,):
+def self_reflection(examples, dataset_description, few_shot_des, constraint, max_reflection=5, ):
     with ThreadPoolExecutor(max_workers=len(examples)) as executor:
-        futures = [executor.submit(reflect_single_example, example, dataset_description, few_shot_des,constraint,max_reflection) for example in examples]
+        futures = [executor.submit(reflect_single_example, example, dataset_description, few_shot_des, constraint,
+                                   max_reflection) for example in examples]
         results = []
         for future in futures:
-            result = future.result() 
-            if result is None:        
-                return None           
+            result = future.result()
+            if result is None:
+                return None
             results.append(result)
     return results
 
-def reflect_single_example(example, dataset_description,few_shot_des,constraint, max_reflection):
+
+def reflect_single_example(example, dataset_description, few_shot_des, constraint, max_reflection):
     try:
-        reflection_list=[]
+        reflection_list = []
         for epoch in range(max_reflection):
-            print(f'----------------------------Self Reflection(Epoch{epoch})----------------------------', "GREEN")
-            example, isgood,reflection_ = reflection_improve_example(example, dataset_description, few_shot_des,constraint)
+            print("*" * 15 + f'Self Reflection(Epoch{epoch})' + "*" * 15, "GREEN")
+            example, isgood, reflection_ = reflection_improve_example(example, dataset_description, few_shot_des,
+                                                                      constraint)
             reflection_list.append({
-                'epoch':epoch,
-                'process':reflection_
-                })
-            if(isgood):
-                example["isgood"]=isgood
-                example["reflection_epoch"]=epoch
-                example['reflection_trajectory']=reflection_list
+                'epoch': epoch,
+                'process': reflection_
+            })
+            if (isgood):
+                example["isgood"] = isgood
+                example["reflection_epoch"] = epoch
+                example['reflection_trajectory'] = reflection_list
                 break
         if not isgood:
-            example["isgood"]=isgood
-            example["reflection_epoch"]=max_reflection
-            example['reflection_trajectory']=reflection_list
-            print('----------------------------The Example is not good after the last epoch----------------------------',"GREEN")
+            example["isgood"] = isgood
+            example["reflection_epoch"] = max_reflection
+            example['reflection_trajectory'] = reflection_list
+            print(
+                "*" * 15 + 'The Example is not good after the last epoch' + "*" * 15,
+                "GREEN")
         return example
     except Exception as e:
-        import traceback; traceback.print_exc();
+        traceback.print_exc()
         print(f'Error during reflection: {e}', "RED")
         #return None
