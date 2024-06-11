74f2398f83eacfb308474489c9b97a1fee6bf032
nauyisu022 <59754221+nauyisu022@users.noreply.github.com>
2024-06-11 21:29:11 +0800

    update

--- unigen/utils/file_process.py
+++ unigen/utils/file_process.py
@@ -1,6 +1,9 @@
 import json
 import os
 
+import yaml
+
+
 def load_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return json.load(f)
@@ -9,6 +12,13 @@ def load_json(file_path):
 def save_json(data, file_path):
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def load_yaml(data, file_path):
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
 
 
 def check_and_rename_file(path):
